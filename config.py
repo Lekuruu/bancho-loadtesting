b8e4396f73e8bf91066f9c25dcd18ba8671b7e3e
Lekuru <contact@lekuru.xyz>
2024-12-01 21:02:42 +0100

    Add config parsing module

---
+++ config.py
@@ -0,0 +1,64 @@
+
+from dataclasses import dataclass
+from typing import List
+
+import json
+import os
+
+@dataclass
+class TcpSettings:
+    Enabled: bool
+    IP: str
+    Port: int
+    Version: int
+    ExecutableHash: str
+
+@dataclass
+class Connection:
+    Domain: str
+    Workers: int
+    TCP: TcpSettings
+
+@dataclass
+class Flags:
+    EnableMessages: bool
+    EnableStatusUpdates: bool
+    EnableSpectating: bool
+    EnableLeaderboardRequests: bool
+
+@dataclass
+class User:
+    Username: str
+    Password: str
+    Messages: list
+    SpectatorTargetId: int
+    MessageTargetChannel: str
+
+@dataclass
+class Leaderboard:
+    BeatmapFilename: str
+    BeatmapHash: str
+    BeatmapsetId: int
+    RequestIntervalMs: int
+
+@dataclass
+class Config:
+    Connection: Connection
+    Flags: Flags
+    Users: List[User]
+    Leaderboard: Leaderboard
+
+def load(path: str = "config.json") -> Config:
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Configuration file not found: '{path}'")
+
+    with open(path, "r") as f:
+        data = json.load(f)
+        data["Connection"]["TCP"] = TcpSettings(**data["Connection"]["TCP"])
+
+    return Config(
+        Connection=Connection(**data["Connection"]),
+        Flags=Flags(**data["Flags"]),
+        Users=[User(**user) for user in data["Users"]],
+        Leaderboard=Leaderboard(**data["Leaderboard"])
+    )
